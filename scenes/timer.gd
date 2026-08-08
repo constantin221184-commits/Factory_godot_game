6c36932eb3b04501d2e0bf8d7999dee14c46a7a7
constantin221184-commits <constantin221184@gmail.com>
2026-08-08 15:05:23 +0200

    fixed bugs

--- scenes/timer.gd
+++ scenes/timer.gd
@@ -1,8 +1,4 @@
-extends Node3D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-var drop_box = preload("res://scenes/drop_box.tscn")
-var spawned: bool = false
+extends Timer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-#func spawnShip() -> void:
-	#animation_player.play("spawn")
-	
